37b42a236a501a504eab0863bf64c328998c9aec
CRECypred <alperemreacar@gmail.com>
2025-07-25 19:52:24 +0300

    Use dynamic port binding for Render

--- server/server.py
+++ server/server.py
@@ -1159,5 +1159,7 @@ def toggle_like(symbol, comment_id):
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
